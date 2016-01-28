f017ff79fd865cdc1dea2caf6399ee44804197cb
Antoine Hars <antoine.hars@gmail.com>
2016-01-28 11:32:28 +0100

    change the type of the field createdAt from Date to String for the formatting of date in ElasticSearch

--- src/main/scala/fr/ippon/spark/streaming/Tweet.scala
+++ src/main/scala/fr/ippon/spark/streaming/Tweet.scala
@@ -1,8 +1,6 @@
 package fr.ippon.spark.streaming
 
-import java.util.Date
-
 /**
   * Created by ahars on 26/01/2016.
   */
-case class Tweet(user: String, text: String, createdAt: Date, language: String)
+case class Tweet(user: String, text: String, createdAt: String, language: String)
